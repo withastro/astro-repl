41b96408d74639edbf7c576abff26c9e9b269e85
Nate Moore <natemoo-re@users.noreply.github.com>
2021-09-17 18:25:10 -0500

    chore: update `@astrojs/compiler` (#8)
    * chore: update `@astrojs/compiler`
    
    * fix: add `renderSlot` util to `astro/internal`
    
    * fix: null slots
    
    * fix: default slot object

--- src/@astro/internal/index.ts
+++ src/@astro/internal/index.ts
@@ -10,7 +10,7 @@ export async function renderAstroComponent(component: InstanceType<typeof AstroC
   return template;
 }
 
-export async function renderToString(result: any, componentFactory: AstroComponentFactory, props: any, children: any) {
+export async function renderToString(result: any, componentFactory: AstroComponentFactory, props: any, children: any = {}) {
   const Component = await componentFactory(result, props, children);
   let template = await renderAstroComponent(Component);
   return template
@@ -153,6 +153,13 @@ setup("${astroId}", {${metadata.hydrateArgs ? `value: ${JSON.stringify(metadata.
 
   return hydrationScript;
 }
+
+export const renderSlot = async (result: any, slotted: string, fallback?: any) => {
+  if (slotted) {
+    return _render(slotted);
+  }
+  return fallback;
+};
 
 export const renderComponent = async (result: any, displayName: string, Component: unknown, _props: Record<string | number, any>, children: any) => {
   Component = await Component;
