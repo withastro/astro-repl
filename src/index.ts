9e95b01995fe6b1e427433735956fc700972ee68
Okiki <okikio.dev@gmail.com>
2021-10-14 19:40:34 +0000

    chore: resolve some reviews

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,6 @@
 import './styles/index.css';
-// import ESBUILD_WORKER_URL from 'worker:./editor/workers/esbuild.ts';
-// import ASTRO_WORKER_URL from 'worker:./editor/workers/astro.ts';
-// import HTML_WORKER_URL from 'worker:./editor/workers/html.ts';
-
-import { WebWorker } from "./utils/WebWorker";
 import { b64Decode } from './utils/b64';
 import render from './ui';
-
-// const esbuildWorker = new Worker(ESBUILD_WORKER_URL, {
-//   name: 'esbuild-worker',
-//   type: 'module',
-// });
-
-// const astroWorker = new Worker(ASTRO_WORKER_URL, {
-//   name: 'astro-worker',
-//   type: 'module',
-// });
-
-// const htmlWorker = new Worker(HTML_WORKER_URL, {
-//   name: 'html-worker',
-//   type: 'module',
-// });
 
 (async () => {
   // Monaco Code Editor
@@ -34,5 +14,5 @@ import render from './ui';
       console.error(`Could not load share URL`);
     }
   }
-  render({ Monaco, /* esbuildWorker, astroWorker, htmlWorker, */ initialModels });
+  render({ Monaco, initialModels });
 })();
