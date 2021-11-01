741460dfcbe894473f8e66153926cc653d0fe8b9
Nate Moore <nate@skypack.dev>
2021-11-01 17:26:27 -0500

    fix: update astro/internal

--- src/utils/astro.ts
+++ src/utils/astro.ts
@@ -1,36 +1,49 @@
 import { renderPage } from '../@astro/internal/index';
 
 export async function renderAstroToHTML(content: string): Promise<string | { errors: string[] }> {
-    const url = `data:application/javascript;base64,${Buffer.from(content).toString('base64')}`;
-    let mod;
-    let html;
-    try {
-        ({ default: mod } = await import(url));
-    } catch (e) {
-        return {
-            errors: [e]
-        }
-    }
-    if (!mod) {
-        return;
-    }
+  const url = `data:application/javascript;base64,${Buffer.from(content).toString('base64')}`;
+  let mod;
+  let html;
+  try {
+    ({ default: mod } = await import(url));
+  } catch (e) {
+    return {
+      errors: [e],
+    };
+  }
+  if (!mod) {
+    return;
+  }
 
-    try {
-        html = await renderPage({
-            styles: new Set(),
-            scripts: new Set(),
-            /** This function returns the `Astro` faux-global */
-            createAstro(props: any) {
-                // const site = location;
-                const url = new URL('http://localhost:3000/')
-                // const canonicalURL = getCanonicalURL(pathname, astroConfig.buildOptions.site || origin)
-                return { isPage: true, site: url, request: { url, canonicalURL: url }, props };
+  try {
+    html = await renderPage(
+      {
+        styles: new Set(),
+        scripts: new Set(),
+        /** This function returns the `Astro` faux-global */
+        createAstro(astroGlobal: any, props: Record<string, any>, slots: Record<string, any> | null) {
+          const url = new URL('http://localhost:3000/');
+          const canonicalURL = url;
+          return {
+            __proto__: astroGlobal,
+            props,
+            request: {
+              canonicalURL,
+              params: {},
+              url,
             },
-        }, await mod, {}, {});
-    } catch (e) {
-        return {
-            errors: [e]
-        }
-    }
-    return html;
+            slots: Object.fromEntries(Object.entries(slots || {}).map(([slotName]) => [slotName, true])),
+          };
+        },
+      },
+      await mod,
+      {},
+      {}
+    );
+  } catch (e) {
+    return {
+      errors: [e],
+    };
+  }
+  return html;
 }
